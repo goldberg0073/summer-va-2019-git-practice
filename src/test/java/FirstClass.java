36d857c8a8ce53470133b91f5e72950b6b0281ca
goldberg0073 <hicham_goldberg@hotmail.com>
2019-11-21 19:51:20 -0500

    secondclass

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,6 +2,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello github");
+        System.out.println("i am working");
+        System.out.println("hellooooo");
     }}
-
 
---
+++ src/test/java/SecondClass.java
@@ -0,0 +1,6 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        String Myname="hisham";
+        System.out.println(Myname);
+    }
+}
